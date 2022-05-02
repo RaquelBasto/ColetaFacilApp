0b7384002a00ab600b9080cf4c8696d95efcda90
RaquelBasto <bastoraquel941@gmail.com>
2022-05-02 18:26:16 -0300

    Ultima alterção 01/05/2022: tela login e tela cadastro: defeitos a corrigir

--- app/src/main/java/br/com/coletafacilapp/FormCadastro.java
+++ app/src/main/java/br/com/coletafacilapp/FormCadastro.java
@@ -1,10 +1,13 @@
 package br.com.coletafacilapp;
 
+import static android.widget.AdapterView.*;
+
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -17,20 +20,46 @@ public class FormCadastro extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Spinner spinner = (Spinner) findViewById(R.id.spinner_languages);
 
+        setContentView(R.layout.activity_form_cadastro);
+        String[] type = new String[]{"teste1","teste2"};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                this,
+                R.layout.drop_down_item,
+                type
+        );
+
+
+        //Spinner spinner = (Spinner) findViewById(R.id.spinner_languages);
+
+        AutoCompleteTextView autoCompleteTextView = findViewById(R.id.spinner);
+        autoCompleteTextView.setAdapter(adapter);
+
+
+        autoCompleteTextView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(FormCadastro.this, autoCompleteTextView.getText().toString(), Toast.LENGTH_SHORT).show();
+
+            }
+        });
         // Create an ArrayAdapter using the string array and a default spinner layout
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.planets_array, android.R.layout.simple_spinner_item);
+
+
+        //ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+          //      R.array.planets_array, android.R.layout.simple_spinner_item);
 
         // Specify the layout to use when the list of choices appears
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
         // Apply the adapter to the spinner
-        spinner.setAdapter(adapter);
+        //spinner.setAdapter(adapter);
 
 
     }
+
 
     public void onItemSelected(AdapterView<?> parent, View view,
                                int pos, long id) {
