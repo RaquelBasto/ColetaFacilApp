30175f2ccbaa7a3576c6d703a2b7d63f174194b3
Laboratorio-Info <bastoraquel941@gmail.com>
2022-05-25 22:20:52 -0300

    Alteração

--- app/src/main/java/br/com/coletafacilapp/fragments/EstudoFragment.java
+++ app/src/main/java/br/com/coletafacilapp/fragments/EstudoFragment.java
@@ -9,33 +9,39 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.RelativeLayout;
 
 import br.com.coletafacilapp.R;
-import br.com.coletafacilapp.mainresources.EcoinfoActivity;
+import br.com.coletafacilapp.mainresources.ColetoresActivity;
 
-public class HomeFragment extends Fragment {
 
-    CardView abrirEcoinfo;
+public class EstudoFragment extends Fragment {
+
+    CardView card;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
 
-        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_estudo, container, false);
 
-        abrirEcoinfo = view.findViewById(R.id.abrirEcoinfo);
-        abrirEcoinfo.setOnClickListener(new View.OnClickListener() {
+        // Iniciando componente
+
+        card = view.findViewById(R.id.cardColetor);
+
+        card.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(getContext(),EcoinfoActivity.class));
+                startActivity(new Intent(getContext(), ColetoresActivity.class));
             }
         });
 
 
         return view;
 
+
+    }
+    public void iniciarComponentes(){
 
     }
 }
